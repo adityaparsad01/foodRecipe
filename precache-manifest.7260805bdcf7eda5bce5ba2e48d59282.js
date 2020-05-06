c079abc73099bfcc6d0f740a967cc77b2df16994
unknown <adityaparsad01@gmail.com>
2020-05-06 18:59:40 +0530

    new build

---
+++ precache-manifest.7260805bdcf7eda5bce5ba2e48d59282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd54d72a881ebf7389c0d9a5d2104f47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c24e2b7faba1f14d782",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "e82c75c5b63ee6832eb8",
+    "url": "/static/css/main.9527593d.chunk.css"
+  },
+  {
+    "revision": "5c24e2b7faba1f14d782",
+    "url": "/static/js/2.afdbdfca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.afdbdfca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82c75c5b63ee6832eb8",
+    "url": "/static/js/main.ec02caef.chunk.js"
+  },
+  {
+    "revision": "d7778145c378f804b44e",
+    "url": "/static/js/runtime-main.94293c76.js"
+  }
+]);
